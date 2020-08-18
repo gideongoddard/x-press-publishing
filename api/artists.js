bc4fa510690d6b97d97fa65147b4055cad35aca1
Gideon Goddard <goddard.gcw@gmail.com>
2020-08-18 19:48:40 +0100

    Added param for artistId

--- api/artists.js
+++ api/artists.js
@@ -4,6 +4,21 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || '../database.sqlite');
 const artistsRouter = express.Router();
 
+artistsRouter.param('artistId', (req, res, next, artistId) => {
+    db.get("SELECT * FROM Artist WHERE id = $artistId", {
+        $artistId = artistId
+    }, (err, row) => {
+        if (err) {
+            next(err);
+        } else if (row) {
+            req.artist = row;
+            next();
+        } else {
+            res.sendStatus(404);
+        }
+    })
+})
+
 artistsRouter.get('/', (req, res, next) => {
     db.all("SELECT * FROM Artist WHERE is_currently_employed = 1", (err, rows) => {
         if (err) {
