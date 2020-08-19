56234a62b5d23b08ad046cf7eff69d7ae2500125
Gideon Goddard <goddard.gcw@gmail.com>
2020-08-19 12:07:25 +0100

    Completed GET route for /:id on artistsRouter; opened empty POST route

--- api/artists.js
+++ api/artists.js
@@ -4,10 +4,9 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || '../database.sqlite');
 const artistsRouter = express.Router();
 
-artistsRouter.param('artistId', (req, res, next, artistId) => {
-    db.get("SELECT * FROM Artist WHERE id = $artistId", {
-        $artistId = artistId
-    }, (err, row) => {
+artistsRouter.param('id', (req, res, next, id) => {
+    const values = {$artistId: id};
+    db.get("SELECT * FROM Artist WHERE id = $artistId", values, (err, row) => {
         if (err) {
             next(err);
         } else if (row) {
@@ -29,7 +28,11 @@ artistsRouter.get('/', (req, res, next) => {
     })
 })
 
-artistsRouter.get('/:artistId', (req, res, next) => {
+artistsRouter.get('/:id', (req, res, next) => {
+    res.status(200).json({artist: req.artist});
+})
+
+artistsRouter.post('/', (req, res, next) => {
     
 })
 
