88f516afe826409096f9cb308bbabda02e71afa0
Gideon Goddard <goddard.gcw@gmail.com>
2020-08-20 11:34:09 +0100

    Completed Artists POST handler

--- api/artists.js
+++ api/artists.js
@@ -33,6 +33,34 @@ artistsRouter.get('/:id', (req, res, next) => {
 })
 
 artistsRouter.post('/', (req, res, next) => {
+    const name = req.body.artist.name;
+    const dateOfBirth = req.body.artist.dateOfBirth;
+    const biography = req.body.artist.biography;
+    const isCurrentlyEmployed = req.body.artist.isCurrentlyEmployed === 0 ? 0 : 1;
+    const sql = `INSERT INTO Artist (name, date_of_birth, biography, is_currently_employed)
+    VALUES ($name, $dateOfBirth, $biography, $isCurrentlyEmployed)`;
+    const values = {
+        $name: name,
+        $dateOfBirth: dateOfBirth,
+        $biography: biography,
+        $isCurrentlyEmployed: isCurrentlyEmployed
+    }
+
+    if (!name || !dateOfBirth || !biography) {
+        return res.sendStatus(400);
+    };
+
+    db.run(sql, values, function(err) {
+        if (err) {
+            next(err);
+        }
+        db.get(`SELECT * FROM Artist WHERE id = ${this.lastID}`, (err, row) => {
+            res.status(201).json({artist: row});
+        });
+    });
+})
+
+artistsRouter.put('/:id', (req, res, next) => {
     
 })
 
